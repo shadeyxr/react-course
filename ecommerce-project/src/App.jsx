cf595bb01a4447c73f91c624af4bb20db5c178db
shadeyxr <89664101+shadeyxr@users.noreply.github.com>
2025-09-25 19:31:30 +0100

    Add Routing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,16 @@
 import {HomePage} from './pages/HomePage.jsx';
+import {Routes,Route} from 'react-router';
+
 import './App.css';
 
 function App() {
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<div>testing</div>} />
+      </Routes>
     </>
   )
 }
