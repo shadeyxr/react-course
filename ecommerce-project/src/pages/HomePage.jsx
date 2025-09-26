adc3c85503e91f789b65cc26452a540a9d4e2a70
shadeyxr <89664101+shadeyxr@users.noreply.github.com>
2025-09-26 23:10:14 +0100

    use axios to get data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
+import axios from 'axios';
 import { Header } from './components/header.jsx'
 import { products } from '../../starting code/data/products.js'
 import './HomePage.css'
 
 export function HomePage() {
+
+  axios.get('http://localhost:3000/api/products')
+    .then((response)=>{
+      console.log(response.data)
+    })
+
   return (
     <>
       <title>Ecommerce</title>
