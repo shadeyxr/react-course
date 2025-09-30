ed18c4e385da23ae134ce9eef6b24ff9e79c939c
shadeyxr <89664101+shadeyxr@users.noreply.github.com>
2025-09-30 19:03:58 +0100

    Load and generate Orders from backend

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage cart={cart} products={products} />} />
         <Route path="checkout" element={<CheckoutPage cart={cart} products={products} />} />
-        <Route path="orders" element={<OrdersPage />} />
+        <Route path="orders" element={<OrdersPage cart={cart} />}  />
         <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </>
