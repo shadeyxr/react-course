5bcfc33a505b36ef1fbb535b39b0cef1c0c6c5a1
shadeyxr <89664101+shadeyxr@users.noreply.github.com>
2025-10-01 13:09:16 +0100

    Seperate payment summary

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,3 +1,4 @@
+import { GeneratePaymentSummary } from './GeneratePaymentSummary.jsx';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import formatMoney from '../../utils/money.js'
@@ -115,44 +116,15 @@ export function CheckoutPage({ cart }) {
             })}
           </div>
 
-          {paymentSummary && (
-            <div className="payment-summary">
-              <div className="payment-summary-title">
-                Payment Summary
-              </div>
-
-
-              <div className="payment-summary-row">
-                <div>Items ({paymentSummary.totalItems}):</div>
-                <div className="payment-summary-money">{`$${formatMoney(paymentSummary.productCostCents)}`}</div>
-              </div>
-
-              <div className="payment-summary-row">
-                <div>Shipping &amp; handling:</div>
-                <div className="payment-summary-money">{`$${formatMoney(paymentSummary.shippingCostCents)}`}</div>
-              </div>
-
-              <div className="payment-summary-row subtotal-row">
-                <div>Total before tax:</div>
-                <div className="payment-summary-money">{`$${formatMoney(paymentSummary.totalCostBeforeTaxCents)}`}</div>
-              </div>
-
-              <div className="payment-summary-row">
-                <div>Estimated tax (10%):</div>
-                <div className="payment-summary-money">{`$${formatMoney(paymentSummary.taxCents)}`}</div>
-              </div>
-
-              <div className="payment-summary-row total-row">
-                <div>Order total:</div>
-                <div className="payment-summary-money">{`$${formatMoney(paymentSummary.totalCostCents)}`}</div>
-              </div>
-
-              <button className="place-order-button button-primary">
-                Place your order
-              </button>
+          <div className="payment-summary">
+            <div className="payment-summary-title">
+              Payment Summary
             </div>
 
-          )}
+            <GeneratePaymentSummary paymentSummary={paymentSummary} />
+            
+          </div>
+
 
 
         </div>
